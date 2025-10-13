bfbe6c285a966642c1504154b4ffa43b345a2412
mskovunov <mskovunov@gmail.com>
2025-10-13 21:49:08 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,3 @@
+// В этом файле хранится URL веб-приложения Google Apps Script.
+  const WEBAPP_URL = 'https://script.google.com/macros/s/AKfycbxiFpwrkbINal0wGhPxMy0wd452HF3Q64yPqXyntLG9HC7gIus6zY2hQoqrcWH_EROf/exec';	// test_meter - test
+  //const WEBAPP_URL = 'https://script.google.com/macros/s/AKfycbwVyyh51sypiCvYZrnVRmqL3spiA4lCkdO9w1EtwpYYIdq-Hu6IUwBRtVGqbZfSgBmO/exec';    // meter_form - DEV
