7917847575b44157e3e16570d19f59c01344077e
mskovunov <mskovunov@gmail.com>
2025-10-22 20:38:19 +0300

    v2025.10.22.1759
    - кастомне модальне вікно виходу
    - авто вибір користувача на головній формі

--- config.js
+++ config.js
@@ -5,5 +5,5 @@
   //const WEBAPP_URL = 'https://script.google.com/macros/s/AKfycbwVyyh51sypiCvYZrnVRmqL3spiA4lCkdO9w1EtwpYYIdq-Hu6IUwBRtVGqbZfSgBmO/exec';    // meter_form - DEV
 
 // Номер версии сборки
-  const BUILD_VERSION = 'v2025.10.21.2159'   
+  const BUILD_VERSION = 'v2025.10.22.1759'   
   
